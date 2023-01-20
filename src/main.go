a4d24752ec94930fd34f153cb29ef37058403701
estiven-lg <estivenlaferre@gmail.com>
2023-01-19 20:07:39 -0600

    ...

--- src/main.go
+++ src/main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hola abismo")
+	const pi float64 = 3.14
+	fmt.Println(pi)
+
+	base := 12
+	fmt.Println(base)
+	var altura int
+	altura = 10
+	fmt.Println(altura)
+	var area int = base * altura
+	fmt.Println(area)
+
 }
